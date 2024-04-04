976c0911427ae6424ffbb5346a5a1bca5375dc65
Sowhum <sohamnaithani@icloud.com>
2024-04-04 16:18:44 -0400

    fixed main, imu+battery

--- src/helpers.h
+++ src/helpers.h
@@ -6,25 +6,33 @@
 #include <Arduino.h>
 #include <gps.h>
 #include <ms5611.h>
+#include <MPU6050.h> 
 #include <SD.h>
 #include "rrc_encoder/src/rrc_encoder.h"
-#include "ICM_20948.h"
+//#include "ICM_20948.h"
 
 
 ////    Defines    ////
 #define RFD_BAUD      115200
 #define RFD_SERIAL    Serial2
 #define GPS_SERIAL    Serial7
-#define BARO_WIRE     Wire1
+#define BARO_WIRE     Wire2
 #define BUZZER        PIN_A12
 #define BUZZER_ENABLE PIN_A13//always apply
-#define IMU_WIRE Wire
-#define AD0_VAL 0
+//#define IMU_WIRE      Wire1
+#define AD0_VAL        0
 #define SERIAL_MONITOR_BAUD 115200
-#define freq 4000
+#define PIN_RED        4
+#define PIN_GREEN      3
+#define PIN_BLUE       2                                                                                                                         
+
+long freq = 1000;
+bool led_debug = false;
 
 ////    Constants    ////
 String logFileName = "log.txt";
+
+/*
 const char outputFormat[] =
     R"""(
 timestamp:   %lu
@@ -33,7 +41,10 @@ T = %lf C    P = %lf mbar
 Location:    %lf, %lf
 
 )""";
-
+*/
+const char outputFormat[] =
+    R"""(%lu,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf
+)""";
 
 ////    Objects    ////
 Ms5611      baro;
@@ -45,14 +56,14 @@ struct
     bool    sdcard = false;
     bool    rfd    = false;
 } partsStates;
-ICM_20948_I2C myICM;
+//ICM_20948_I2C myICM;
 
 ////    Functions declairations    ////
 void transmit   (double data, uint8_t header, uint32_t time);
 void debug      (void);
 void buzzFor    (unsigned int time_ms, unsigned int after = 0);
 void setParts   (void);
-void getScaledAGMT(ICM_20948_I2C *sensor);
+//void getScaledAGMT(ICM_20948_I2C *sensor);
 
 ////    Functions definitions    ////
 void transmit(double data, uint8_t header, uint32_t time)
@@ -77,28 +88,84 @@ void buzzFor(unsigned int time_ms, unsigned int after)
         delay(time_ms);
         digitalWrite(BUZZER, LOW);
         delay(after);
+        led_debug = true; 
     }
 }
 
+
+
+void errorLED(int errorcode)
+{
+  unsigned long time = 0;
+  unsigned long delta = 0;
+  time = millis();
+  
+  switch(errorcode) {
+    case 1:      
+      analogWrite(PIN_RED,   255);
+      analogWrite(PIN_GREEN, 0);
+      analogWrite(PIN_BLUE,  0);
+      break;
+  
+    case 2:
+
+      analogWrite(PIN_RED,   0);
+      analogWrite(PIN_GREEN, 255);
+      analogWrite(PIN_BLUE,  0);
+      break;
+
+    case 3:
+
+      analogWrite(PIN_RED,   0);
+      analogWrite(PIN_GREEN, 0);
+      analogWrite(PIN_BLUE,  255);
+      break;
+
+    case 4:
+
+      analogWrite(PIN_RED,   0);
+      analogWrite(PIN_GREEN, 200);
+      analogWrite(PIN_BLUE,  255);
+
+      break;
+    
+    case 5:
+
+      analogWrite(PIN_RED,   200);
+      analogWrite(PIN_GREEN, 12);
+      analogWrite(PIN_BLUE,  0);
+
+      break;
+      case 6:
+            analogWrite(PIN_RED,   0);
+
+ analogWrite(PIN_GREEN, 0);
+      analogWrite(PIN_BLUE,  0);
+
+      break;
+      
+
+  }
+}
 
 void setParts(void)
 {
     // init MS5611
     if(!partsStates.baro)
-    {
-        if(baro.init(&BARO_WIRE))
-        {
+        {        
+        if(baro.init(&BARO_WIRE)){
+
             partsStates.baro = false;
             Serial.println("MS5611 init error");
-            buzzFor(500, 50);
+           // buzzFor(500, 50);
             buzzFor(50, 50);
-            buzzFor(50, 250);
+            //buzzFor(50, 250);
         }
         else
         {
             partsStates.baro = true;
             Serial.println("MS5611 init OK");
-            buzzFor(50, 100);
+            //buzzFor(50, 100);
         }
     }
 
@@ -106,10 +173,9 @@ void setParts(void)
     if(!partsStates.gps)
     {
         gps.init(&GPS_SERIAL, 9600);
-
         partsStates.gps = true;
         Serial.println("GPS init OK");
-        buzzFor(50, 100);
+        //buzzFor(50, 100);
     }
 
 
@@ -117,10 +183,9 @@ void setParts(void)
     if(!partsStates.rfd)
     {
         RFD_SERIAL.begin(RFD_BAUD);
-
         partsStates.rfd = true;
         Serial.println("RFD init OK");
-        buzzFor(50, 100);
+        //buzzFor(50, 100);
     }
 
 
@@ -131,28 +196,31 @@ void setParts(void)
         {
             partsStates.sdcard = false;
             Serial.println("SD Card init error");
-            buzzFor(500, 50);
-            buzzFor(50, 50);
-            buzzFor(50, 50);
-            buzzFor(50, 250);
+           // buzzFor(500, 50);
+           // buzzFor(50, 50);
+           // buzzFor(50, 50);
+           // buzzFor(50, 250);
 
         }
         else
         {
             partsStates.sdcard = true;
             Serial.println("SD Card init OK");
-            buzzFor(50, 100);
+           // buzzFor(50, 100);
         }
     }
 }
 
 
+
 #endif  //  #ifndef __RRC_HELPER_FUNCS__
 
 //IMU
+/*
 void getScaledAGMT(ICM_20948_I2C *sensor,float *x,float *y,float *z) //array [0]
 {
   *x = sensor->accX();
   *y = sensor->accY();
   *z = sensor->accZ();
 }
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,49 @@
 ////    Includes    ////
 #include <Arduino.h>
 #include <math.h>
+#include <Wire.h>
 #include "helpers.h"
+#include <Adafruit_INA260.h>
 bool rfd_comms_ini = true;
 
 double temp, pres, lon, lat;
 // float PDOP, VDOP, HDOP;
+MPU mpu;
+AStruct imu_acc;
+Adafruit_INA260 ina260 = Adafruit_INA260();
+
 float x, y, z, r = 0;
 uint32_t start;
 uint8_t counter = 0;
 ////    Initilization setup    ////
 void setup(void)
 {
+
     pinMode(BUZZER, OUTPUT);
     pinMode(BUZZER_ENABLE, INPUT_PULLUP);
+
+    pinMode(PIN_RED, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+    pinMode(PIN_BLUE, OUTPUT);
+
     buzzFor(1000, 1000);
+    Wire.begin();
+    Wire1.begin();
+    Wire2.begin();
+    ina260.begin();
+    mpu.pwr_setup();
+    mpu.acc_setup(1);
+    if (!ina260.begin())
+    {
+        buzzFor(100, 100);
+        errorLED(1);
+        errorLED(0);
+        delay(100);
+        errorLED(1);
+        errorLED(0);
+        delay(100);
+        errorLED(1);
+    }
 
     // start serial monitor
     Serial.begin(SERIAL_MONITOR_BAUD);
@@ -30,40 +59,41 @@ void setup(void)
 
     setParts();
 
-    RFD_SERIAL.printf("idle\n");
+    // basically rfd uses the most power & since rocket is going to be idle on platorm for awhile dont do anything until bit is sent
+    /*
+    while(true)
+     {
+        //RFD_SERIAL.printf("idle\n");
+         if(RFD_SERIAL.available())
+         {
+             String command = RFD_SERIAL.readStringUntil('\n');
+             command.toLowerCase();
 
-    
-    IMU_WIRE.begin();
-    IMU_WIRE.setClock(400000);
+             if(command.equals("launch"))
+             {
+                 break;
+             }
+             else
+             {
+                 Serial.printf("command \"%s\" unrecognized\n", command.c_str());
+             }
+         }
+     }
 
-    bool initialized = false;
-    if (!initialized)
-    {
-        myICM.begin(IMU_WIRE, AD0_VAL);
 
-        Serial.print(F("Initialization of the sensor returned: "));
-        Serial.println(myICM.statusString());
-        if (myICM.status != ICM_20948_Stat_Ok)
-        {
-            Serial.println("Trying again...");
-            //         delay(500);
-        }
-        else
-        {
-            initialized = true;
-        }
-    }
+     Serial.printf("launching\n");
+    */
 }
 
 ////    Main loop    ////
 void loop(void)
 {
     static uint32_t timestamp = 0;
-
     char string[256] = {0};
     start = millis(); // store current time
 
     setParts();
+    float volt_battery = ina260.readBusVoltage();
 
     // read MS5611
     if (partsStates.baro)
@@ -71,51 +101,62 @@ void loop(void)
         if (baro.getTempPress(&temp, &pres))
         {
             Serial.printf("baro read failed\n");
+            errorLED(1);
+            buzzFor(250, 250);
+
+            if (led_debug)
+            {
+                errorLED(6);
+                delay(250);
+
+                errorLED(1);
+            }
+            else 
+            {led_debug=false; 
+            }
         }
     }
+
+    else
+    {
+        errorLED(1);
+        delay(250);
+    }
+
+    if(mpu.getErr()){
+        mpu.pwr_setup();
+        mpu.acc_setup(1);
+        Serial.printf("imu read failed\n");
+        errorLED(2);
+        buzzFor(250,250);
+
+        delay(100);
+        mpu.get_acc(1,&imu_acc);
+    }
+
+    else{
+        mpu.get_acc(1,&imu_acc);
+    }
+
     int numbSat, quality;
     char opMode;
     float HDOP, PDOP, sigStrength;
     // read gps
+
     if (partsStates.gps)
     {
-        if (gps.read_RMC(&lon, &lat, freq))
+        if (gps.read_RMC(&lon, &lat, 4000))
         {
             Serial.printf("gps RMC read failed\n");
+            errorLED(3);
+            // buzzFor(250,250);
         }
-        /*if (gps.read_GSA(&opMode, &HDOP, &PDOP, 1000))
-        {
-            Serial.println(F("GSA read fail"));
-        }
-        delay(100);
-        if (gps.read_GSV(&numbSat, &sigStrength, 1000))
-        {
-            Serial.println(F("GSV read fail"));
-        }
-        delay(100);
-        if (gps.read_GGA(&quality, 1000))
-        {
-            Serial.println(F("GGA read fail"));
-        }*/
-    }
-
-    if (myICM.dataReady())
-    {
-        myICM.getAGMT();
-        getScaledAGMT(&myICM, &x, &y, &z);
-        r = sqrt(x * x + y * y + z * z);
-        // delay(500);
-    }
-    else
-    {
-        Serial.println("Waiting for data");
-        // delay(500);
     }
 
     // print stuff to serial and SD card (need to call array for xyz, use equation for r here)
     sprintf(
         string, outputFormat,
-        timestamp++, x / 1000, y / 1000, z / 1000, r, temp, pres, lat, lon);
+        timestamp++, volt_battery, imu_acc.XAxis, imu_acc.YAxis, imu_acc.ZAxis, temp, pres, lat, lon);
 
     Serial.printf("%s", string);
 
@@ -131,19 +172,19 @@ void loop(void)
         else
         {
             Serial.printf("error opening %s\n", logFileName.c_str());
+            errorLED(3);
             partsStates.sdcard = false;
         }
     }
 
     // encode and transmit data
 
-    if (rfd_comms_ini == true)// basically rfd uses the most power & since rocket is going to be idle on platorm for awhile dont do anything until bit is sent
-    
+    if (rfd_comms_ini == true)
     {
 
         String command = RFD_SERIAL.readStringUntil('\n');
         command.toLowerCase();
-        if (command.equals("launch"))
+        /*if (command.equals("launch"))
         {
             rfd_comms_ini = false;
             Serial.printf("launching\n");
@@ -151,40 +192,37 @@ void loop(void)
         }
         else
         {
-            if (counter<=30) // 30 seconds total ; triggers once a second
+            if (counter <= 5) // 5 seconds total ; triggers once a second
             {
                 RFD_SERIAL.printf("idle\n");
-				   RFD_SERIAL.printf("idle\n");
-            RFD_SERIAL.printf("idle\n");
- 
-
                 delay(1000);
-              
+
                 counter++;
             }
-            else if (counter>=9999)
+            else if (counter >= 9999)
             {
-                counter =0; // reset the counter
+                counter = 0; // reset the counter
             }
-            
-          // Serial.printf("command \"%s\" unrecognized\n", command.c_str());
-   
-              
+
+            Serial.printf("command \"%s\" unrecognized\n", command.c_str());
         }
     }
     else
     {
+        
         transmit(x, RRC_HEAD_ACC_X, timestamp);
         transmit(y, RRC_HEAD_ACC_Y, timestamp);
         transmit(z, RRC_HEAD_ACC_Z, timestamp);
         transmit(temp, RRC_HEAD_TEMP, timestamp);
         transmit(pres, RRC_HEAD_PRESS, timestamp);
         transmit(lat, RRC_HEAD_GPS_LAT, timestamp);
-        transmit(lon, RRC_HEAD_GPS_LONG, timestamp);
+        transmit(lon, RRC_HEAD_GPS_LONG, timestamp);*/
+        RFD_SERIAL.println(outputFormat);
+        // transmit(volt_battery,RRC_HEAD_BATT_V,timestamp);
         Serial.println("transmitting");
     }
-    while (millis() - start <= freq) // print every 4 second
+    while (millis() - start <= 4000) // print every 4 second
     {
-        ;
+        
     }
 }
